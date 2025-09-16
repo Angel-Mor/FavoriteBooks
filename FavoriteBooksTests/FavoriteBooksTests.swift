0d09ae0c34e521e25837b93df79af96921c610ff
Angel-Mor <angel4761.be23@chitkara.edu.in>
2025-09-16 23:59:48 +0530

    FavoriteBooks

---
+++ FavoriteBooksTests/FavoriteBooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBooksTests.swift
+//  FavoriteBooksTests
+//
+//  Created by Angel Mor on 16/09/25.
+//
+
+import Testing
+@testable import FavoriteBooks
+
+struct FavoriteBooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
